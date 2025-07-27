0f72a56b488632869231af1ad0c08908e2a0e4ec
VICTOR GABRIEL SANTOS ALVES <victor.alves@dcomp.ufs.br>
2025-07-27 18:38:50 -0300

    Create HelloWorld.C

---
+++ HelloWorld.C
@@ -0,0 +1,24 @@
+// Para Comentários
+// Núcleo do C é pequeno, muito do que é feito é através de bibliotecas
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!");
+}
+
+// stdio: "biblioteca de entrada e saída padrão" (do compilador)
+// STanDard Input and Output
+// Fornece a função printf()
+
+// Função: recebe 1+ argumentos e retorna 1 valor (corpo entre {})
+// main(): ponto de entrada de qualquer programa (não recebe argumentos e retorna um inteiro)
+// Identificação dessa característica: void (argumento) e int (valor de retorno)
+
+// Precisa ter ponto e vírgula
+
+
+
+
+
+
